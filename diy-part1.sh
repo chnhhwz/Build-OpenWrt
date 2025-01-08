f0ce50b7f31057a1c05891b85fd36cd2f093dedf
chnhhwz <103352599+chnhhwz@users.noreply.github.com>
2025-01-09 04:17:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,13 +9,3 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-
-# 添加OpenClash插件
-# echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-
-# 添加PassWall插件
-# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-
-# 添加kenzok8插件集合（包含SSRplus、OpenClash、PassWall）
-# echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
